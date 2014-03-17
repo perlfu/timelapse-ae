e7ba684602353495825a11625f76147f598d702a
Carl Ritson <cgr@kent.ac.uk>
2014-03-17 17:29:50 +0000

    Start thinking about plotting measurement data.

---
+++ plot-measures.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import pickle
+
+def main(args):
+    # load measures
+    # for each measure (img_type + stat)
+    #   plot scatter graph over time
+    #   plot tone (in 3D?)
+    pass
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    sys.exit(0)
