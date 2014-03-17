b28227d0f6c3f86bd97671b316eebce87c4744be
Carl Ritson <cgr@kent.ac.uk>
2014-03-17 14:34:18 +0000

    Continue work on building averages.

--- avg.py
+++ avg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-#from wand.image import Image
 import astral
 import datetime
 import os, os.path
@@ -8,8 +7,7 @@ import re
 import subprocess
 import sys
 
-#def average(files, output):
-#    subprocess.call(['composite'] + files + [output]
+AVGIMG = 'avgimg'
 
 def fn_to_date(fn):
     year = int(fn[0:2]) + 2000
@@ -67,11 +65,11 @@ def build_mapping(aloc, src_path, dst_path, files):
         }
         mapping[fn] = details
 
-        membership = [ 
-            'day/' + day + '/all', 
-            'day/' + day + '/' + period,
-            'month/' + month + '/all', 
-            'month/' + month + '/' + period
+        membership = [
+            os.path.join('day', day, 'all'),
+            os.path.join('day', day, period),
+            os.path.join('month', month, 'all'),
+            os.path.join('month', month, period)
         ]
         for i in range(-2,3):
             if i < 0:
@@ -80,8 +78,8 @@ def build_mapping(aloc, src_path, dst_path, files):
                 other = day_string(dt + datetime.timedelta(+i))
             
             membership += [
-                '5day/' + other + '/all',
-                '5day/' + other + '/period'
+                os.path.join('5day', other, 'all'),
+                os.path.join('5day', other, period)
             ]
 
         for entry in membership:
@@ -102,6 +100,7 @@ def generate_base_img(dst_path, details, img_type):
         subprocess.call(['convert', src, '-adaptive-resize', '1920', dst])
     else:
         assert(0)
+    d[img_type + '_mtime'] = os.path.getmtime(dst)
 
 def preprocess(mapping, dst):
     for (fn, d) in mapping.items():
@@ -120,7 +119,92 @@ def preprocess(mapping, dst):
             generate_base_img(dst, d, 'hd')
         else:
             d['hd_mtime'] = os.path.getmtime(os.path.join(dst, ld))
-            
+
+def generate_average(dst, path, srcs, label='raw'):
+    subprocess.call([AVGIMG, os.path.join(dst, path, label)] + srcs)
+
+def build_averages(averages, dst, img_type='ld'):
+    result = {}
+    for (path, srcs) in averages.items():
+        path = os.path.join('avg', path)
+
+        most_recent = 0
+        for src in srcs:
+            src_mtime = src[img_type + '_mtime']
+            if src_mtime > most_recent:
+                most_recent = src_mtime
+
+        mtime = 0
+        if not os.path.exists(os.path.join(dst, path)):
+            os.makedirs(os.path.join(dst, path))
+        elif os.path.exists(os.path.join(dst, path, 'raw-avg.png')):
+            mtime = os.path.getmtime(os.path.join(dst, path, 'raw-avg.png'))
+
+        if most_recent >= mtime:
+            src_paths = []
+            for src in srcs:
+                src_paths.append(os.path.join(dst, src[img_type]))
+            generate_average(dst, path, src_paths)
+            mtime = os.path.getmtime(os.path.join(dst, path, 'raw-avg.png'))
+
+        result[path] = mtime
+
+    return result
+
+def reprocess_averages(averages, dst):
+    for (path, mtime) in averages.items():
+        for t in ['geoavg', 'min', 'max']:
+            src = os.path.join(dst, path, 'raw-' + t + '.png')
+            gen = {
+                t + "-eq.png": ['-equalize'],
+                t + "-eq-gray.png": ['-equalize', '-separate', '-average'],
+                t + "-gray-eq.png": ['-separate', '-average', '-equalize']
+            }
+            for (fn, opts) in gen.items():
+                fpath = os.path.join(dst, path, fn)
+                if (not os.path.exists(fpath)) or (mtime > os.path.getmtime(fpath)):
+                    print 'generate', os.path.join(path, fn)
+                    subprocess.call(['convert', src] + opts + [fpath])
+
+def fingerprint(path):
+    ppm = subprocess.check_output(['convert', path, '-gravity', 'center', '-crop', '80%', '-scale', '3x3!', '-compress', 'none', '-depth', '16', 'ppm:'])
+    lines = ppm.split("\n")
+    raw = (" ".join(lines[3:len(lines)])).split(" ")
+    data = []
+    for v in raw:
+        if len(v) > 0:
+            data.append(int(v))
+    return data
+
+def rsd(a, b):
+    d = 0.0
+    for i in range(min(len(a), len(b))):
+        d += (float(a[i]) - float(b[i])) ** 2
+    return math.sqrt(d)
+
+def comparison(src, dst):
+    result = {}
+    for metric in ['MSE', 'PSNR']:
+        vsl = subprocess.check_output(['compare', '-metric', metric, src, dst, 'null:'])
+        m = re.match(r'.*\((.+)\)')
+        if m:
+            v = float(m.group(1))
+        else:
+            v = float(vsl)
+        result[metric] = v
+    return result
+
+def day_list(mapping):
+    days = {}
+    for (fn, d) in mapping.items():
+        days[d['day']] = True
+    return sorted(days.keys())
+
+def measure_day(path, day, img_type):
+    # get tone measures
+    # compare to: previous day, 5-day window, month
+    pass
+
 def find_files(path):
     file_re = re.compile(r'\d{12}\.jpg')
     raw_files = os.listdir(path)
@@ -143,8 +227,13 @@ def main(args):
         dst_path = args[1]
         files = find_files(src_path)
 
-        (mapping, average) = build_mapping(aloc, src_path, dst_path, files)
+        (mapping, averages) = build_mapping(aloc, src_path, dst_path, files)
         preprocess(mapping, dst_path)
+        mtimes = build_averages(averages, dst_path)
+        reprocess_averages(mtimes, dst_path)
+    else:
+        print 'avg <src> <dst>'
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+    sys.exit(0)
